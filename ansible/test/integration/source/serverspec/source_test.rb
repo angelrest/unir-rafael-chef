745017b8fab0c84f9899a87850e130e067ec119e
angelrest <45704368+angelrest@users.noreply.github.com>
2023-01-07 14:56:38 -0500

    últimos cambios

--- ansible/test/integration/source/serverspec/source_test.rb
+++ ansible/test/integration/source/serverspec/source_test.rb
@@ -1,34 +1,15 @@
-# Chef InSpec test for recipe ansible::source
+# require 'spec_helper'
 
-# The Chef InSpec reference, with examples and extensive documentation, can be
-# found at https://docs.chef.io/inspec/resources/
-
-# unless os.windows?
-#   # This is an example test, replace with your own test.
-#   describe user('root'), :skip do
-#     it { should exist }
-#   end
+# if os[:family] == 'ubuntu'
+#     describe package('ansible') do
+#         it {should be_installed}
+#     end
 # end
 
-# # This is an example test, replace it with your own test.
-# describe port(80), :skip do
-#   it { should_not be_listening }
+# # realizamos un ping sobre el equipo con ansible--------------
+# describe command('ansible localhost -m ping ') do
+#   its('stdout') { should match /bin/ }
+#   its('exit_status') { should eq 0 }
+#   its('stderr') { should eq '' }
 # end
-
-# execute 'host' do
-#   command 'cat /etc/ansible/hosts'
-#   live_stream true
-#   action :run
-# end
-
-require'spec_helper'
-
-if os[:family] == 'ubuntu'
-    describe package('ansible') do
-        it {should be_installed}
-    end
-end
-
-describe command('ansible localhost -m setup ') do
-  its('stdout') { should match /source/ }
-end
+# # ------------------------------------------------------------
